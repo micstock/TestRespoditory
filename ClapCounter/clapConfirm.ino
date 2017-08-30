36b6897bfe6bfb636e69a87386bd80465c72346a
tyiu-VAIO\t yiu <kendotwins@gmail.com>
2017-08-30 20:35:14 +1000

    tidy up comments & formatting

--- ClapCounter/clapConfirm.ino
+++ ClapCounter/clapConfirm.ino
@@ -1,33 +1,34 @@
-<<<<<<< HEAD
-//see your comment
-=======
 /*
     clapConfirm() will confirm a clap is identified
 */
->>>>>>> 2f5355514ef98b425676f9697585ed59c7a390d4
 
 void clapConfirm(){
   
   flagConfirm =1;
-  if (timebase>0){ timebase--;}
-else{
-  if(clapCounter>0){   
-    if(clapCounter==1){
-      Serial.println("    Confirmed Clap");
-      digitalWrite(BLUEpin,LOW);
-      digitalWrite(REDpin,LOW);
-      digitalWrite(GREENpin,LOW);
-      flagConfirm=0;
-      //delay(1000);
+  if (timebase>0)
+  { 
+      timebase--;
+  }
+  else{
+      if(clapCounter>0)
+      {   
+        if(clapCounter==1)
+        {
+            Serial.println("    Confirmed Clap");
+            digitalWrite(BLUEpin,LOW);
+            digitalWrite(REDpin,LOW);
+            digitalWrite(GREENpin,LOW);
+            flagConfirm=0;
+            //delay(1000);
+        }
+        
       }
-    
-     }
 
      clapCounter=0;
   } 
   Serial.print("clapConfirm clapCounter==");
   Serial.println(clapCounter);
- Serial.println(timebase);
-delay(1000);
+  Serial.println(timebase);
+  delay(1000);
 }
 
