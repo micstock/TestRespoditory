8e5f0c69660afdaca9dcd7a49710f24e06dab8d2
PC\Administrator <mic_stock@yahoo.com.su>
2017-09-06 12:34:22 +1000

    correct header

--- ClapCounter/clapConfirm.ino
+++ ClapCounter/clapConfirm.ino
@@ -1,31 +1,42 @@
+<<<<<<< HEAD
 //see your commen
 /*
     clapConfirm() will confirm a clap is identified
 */
 
+=======
+/*
+    clapConfirm() will confirm a clap is identified
+*/
+>>>>>>> 36b6897bfe6bfb636e69a87386bd80465c72346a
 
 void clapConfirm(){
   
   flagConfirm =1;
-  if (timebase>0){ timebase--;}
-else{
-  if(clapCounter>0){   
-    if(clapCounter==1){
-      Serial.println("    Confirmed Clap");
-      digitalWrite(BLUEpin,LOW);
-      digitalWrite(REDpin,LOW);
-      digitalWrite(GREENpin,LOW);
-      flagConfirm=0;
-      //delay(1000);
+  if (timebase>0)
+  { 
+      timebase--;
+  }
+  else{
+      if(clapCounter>0)
+      {   
+        if(clapCounter==1)
+        {
+            Serial.println("    Confirmed Clap");
+            digitalWrite(BLUEpin,LOW);
+            digitalWrite(REDpin,LOW);
+            digitalWrite(GREENpin,LOW);
+            flagConfirm=0;
+            //delay(1000);
+        }
+        
       }
-    
-     }
 
      clapCounter=0;
   } 
   Serial.print("clapConfirm clapCounter==");
   Serial.println(clapCounter);
- Serial.println(timebase);
-delay(1000);
+  Serial.println(timebase);
+  delay(1000);
 }
 
