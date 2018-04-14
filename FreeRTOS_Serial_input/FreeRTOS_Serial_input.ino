91c08c504b1c6e5a83bf4d103b24a743a9358a97
micstock <31384573+micstock@users.noreply.github.com>
2018-04-15 08:28:46 +1000

    Add files via upload
    Just Testing Serial input issue with FreeRTOS

---
+++ FreeRTOS_Serial_input/FreeRTOS_Serial_input.ino
@@ -0,0 +1,24 @@
+#include <Arduino_FreeRTOS.h>
+
+void setup() {
+  Serial.begin(9600); 
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+}
+String in_string;
+int in_num;
+int index;
+void loop()
+{
+  Serial.println("input now..");
+  while (!Serial.available()) {}; //waiting for input..
+  in_string = Serial.readString();
+  in_num = in_string.toInt();
+
+  Serial.print(index);Serial.print(" in_num =.");Serial.print(in_num);
+  Serial.print(" in_string = "); Serial.println(in_string);
+  index++;
+  delay(200);
+}
+
